3e4df8427d909e8c65c692d87143e0f9c4e8d87f
Arun <arun@arun-synqua.(none)>
2013-06-13 22:40:58 +0530

    Added Layout

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>WebExpert</title>
+		<link rel="stylesheet" type="text/css" href="main.css" />
+	</head>
+	<body style="background-image: url('image/background6.jpg');">
+		<div style="color: rgb(185, 71, 7); Trapian Pro Bold;  margin-left: 7%; margin-bottom: 3%; line-height: 1%;; ">
+			<h1 >@Developers</h1>
+			<h5 style="margin-left: 4%;margin-top: 3%;">Beauty of Simplicity</h5>
+		</div>
+		<div class="margin1" style="padding: 5%; height: 63% ;width: 80%;  border-style: double; border-color: brown; border-radius: 2% 10% 10% 2%; margin-left: 5%;">
+			
+			
+		</div>
+	</body>
+</html>
