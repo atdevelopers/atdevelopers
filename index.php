1ade55d8623663b3fba8ac576fa7f294f0ce7082
Madhu <madhu1190@gmail.com>
2013-06-14 01:24:30 +0430

    Added Navigation bar

--- index.php
+++ index.php
@@ -3,15 +3,56 @@
 	<head>
 		<title>WebExpert</title>
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/navigation.css" />
+
+		<div class="container">
+			<div class="site_title">
+				<h1 >@Developers</h1>
+				<h5 class="tag_line">Beauty of Simplicity</h5>
+			</div>
+            <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a class="hsubs" href="#">Menu 1</a>
+                    <ul class="subs">
+                        <li><a href="#">Submenu 1</a></li>
+                        <li><a href="#">Submenu 2</a></li>
+                        <li><a href="#">Submenu 3</a></li>
+                        <li><a href="#">Submenu 4</a></li>
+                        <li><a href="#">Submenu 5</a></li>
+                    </ul>
+                </li>
+                <li><a class="hsubs" href="#">Menu 2</a>
+                    <ul class="subs">
+                        <li><a href="#">Submenu 2-1</a></li>
+                        <li><a href="#">Submenu 2-2</a></li>
+                        <li><a href="#">Submenu 2-3</a></li>
+                        <li><a href="#">Submenu 2-4</a></li>
+                        <li><a href="#">Submenu 2-5</a></li>
+                        <li><a href="#">Submenu 2-6</a></li>
+                        <li><a href="#">Submenu 2-7</a></li>
+                        <li><a href="#">Submenu 2-8</a></li>
+                    </ul>
+                </li>
+                <li><a class="hsubs" href="#">Menu 3</a>
+                    <ul class="subs">
+                        <li><a href="#">Submenu 3-1</a></li>
+                        <li><a href="#">Submenu 3-2</a></li>
+                        <li><a href="#">Submenu 3-3</a></li>
+                        <li><a href="#">Submenu 3-4</a></li>
+                        <li><a href="#">Submenu 3-5</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Menu 4</a></li>
+                <li><a href="#">Menu 5</a></li>
+                <li><a href="#">Menu 6</a></li>
+                <li><a href="http://www.script-tutorials.com/css3-multicolor-menu/">Back</a></li>
+            </ul>
+
+        </div>
+
 	</head>
 	<body style="background-image: url('image/background6.jpg');">
-		<div class="sit_title">
-			<h1 >@Developers</h1>
-			<h5 class="tag_line">Beauty of Simplicity</h5>
-		</div>
-		<div class="margin1" >
-			
-			
+		<div class="margin1" >			
 		</div>
 	</body>
 </html>
