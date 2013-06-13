88f8b2dc2920c1aef020dfcc2cda5768bc280efd
Madhu <madhu1190@gmail.com>
2013-06-13 23:27:34 +0430

    Madhu: Moved all styles to external css

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
 	<head>
 		<title>WebExpert</title>
-		<link rel="stylesheet" type="text/css" href="main.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
 	</head>
 	<body style="background-image: url('image/background6.jpg');">
-		<div style="color: rgb(185, 71, 7); Trapian Pro Bold;  margin-left: 7%; margin-bottom: 3%; line-height: 1%;; ">
+		<div class="sit_title">
 			<h1 >@Developers</h1>
-			<h5 style="margin-left: 4%;margin-top: 3%;">Beauty of Simplicity</h5>
+			<h5 class="tag_line">Beauty of Simplicity</h5>
 		</div>
-		<div class="margin1" style="padding: 5%; height: 63% ;width: 80%;  border-style: double; border-color: brown; border-radius: 2% 10% 10% 2%; margin-left: 5%;">
+		<div class="margin1" >
 			
 			
 		</div>
